ac7217edd5f14ac574553c967aa98508ebad3562
Youssef Elkomy <youssefelkomy500@gmail.com>
2024-11-29 15:04:29 +0200

    Configure main.yml file

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 
-const app = express();
+
 const PORT = 5000;
-const HOST = "localhost";
 
-app.get('/', (req, res) => {
-    res.send("<h1>Eng. Nada hya eltop</h1>");
-})
+const app = express.application();
 
-app.listen(PORT, () => {
-    console.log("The app is up & running ya 3sl :)");
-});
+app.
